146595bc44e56b45bae08eff870eff3d159a6372
PratyushPoddar07 <PratyushPoddar07@users.noreply.github.com>
2026-08-05 18:17:20 +0530

    concat

--- 6.sql
+++ 6.sql
@@ -39,3 +39,60 @@ from
 	dim_date; 
 
 
+-- 3
+SELECT 
+	date,
+    date_format(date,"%W %M %e %Y") as new_formatedDate
+FROM
+	dim_date;
+    
+
+-- TYPE CASTING ---
+-- data type should be same for joins
+SELECT
+	 customer_key,
+     CAST(customer_key AS CHAR(1000))
+FROM
+	dim_customer;
+
+	
+-- STRING FUNCTIONS ---
+-- 1 [concat]
+SELECT
+	concat(first_name," ",last_name) as full_name
+FROM 
+	dim_customer;
+-- 2 [size of the country]
+SELECT * FROM dim_customer;
+SELECT
+	country,
+	LENGTH(country) AS COUNTRY_SIZE
+FROM
+	dim_customer;
+    
+-- 3 [make it in lower and upper case , FETCH REPLACE, AND SLICING]
+
+SELECT
+	LOWER(city) as lower_city,
+    upper(CITY) AS UPPER_CITY,
+    substring(EMAIL,1,4), -- FETCH IN RANGE
+	replace(EMAIL,'.net','.com'), -- USED TO REPLACE 
+    COUNTRY,
+    left(COUNTRY,3), -- FOR SLICING FROM START
+    RIGHT(COUNTRY,3) -- FOR SLICING FRO END
+FROM
+	dim_customer;
+
+-- 4 [FOR REVERSE and REPEAT]
+SELECT
+	REPEAT(first_name,2),
+	reverse(country) as ulta_country
+from
+	dim_customer;
+
+
+-- 5 [ SUPPOSE U HAVE 5 COLUMNS AND U WANT TO COMBINE THEM AND MAKE 1 COLUMN SEPARATED BY SPACE]
+SELECT
+	concat_ws(' - ',first_name,last_name,country)
+FROM
+	dim_customer;
